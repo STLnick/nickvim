06f3588c9e1a2674ddc0af5e815e8e31a2fafc26
Nick Ray <nick@curate.co>
2023-09-11 14:52:09 -0500

    Add eslint as fixer and linter

--- after/plugin/ale.lua
+++ after/plugin/ale.lua
@@ -1,8 +1,11 @@
 vim.g.ale_fixers = {
     css = {'prettier', 'trim_whitespace'},
-    javascript = {'prettier', 'trim_whitespace'},
-    svelte = {'prettier', 'trim_whitespace'},
-    vue = {'prettier', 'trim_whitespace'}
+    javascript = {'prettier', 'eslint', 'trim_whitespace'},
+    svelte = {'prettier', 'eslint', 'trim_whitespace'},
+    vue = {'prettier', 'eslint', 'trim_whitespace'}
+}
+vim.g.ale_linters = {
+    javascript = {'eslint'},
 }
 vim.g.ale_fix_on_save = 1
 vim.g.ale_linters_explicit = 1
